2d5944ef49a343b95b83ffe0f8ab9108253cd831
Adriano Nascimento <ns.adriano@gmail.com>
2021-04-06 20:37:53 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -14,6 +15,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);		
@@ -37,18 +39,25 @@ public class Program {
 		}
 		
 		
-		System.out.println("\n=== TEST 4: seller insert ===");
+		/*System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Seu Madruga", "madruga@gmail.com", new Date(), 3000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-		
+		*/
 		
 		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Kiko");
 		sellerDao.update(seller);
 		System.out.println("Updated complete!");
-
+		
+		
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Entre com o codigo para deleção: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete!");
 	}
 
 }
