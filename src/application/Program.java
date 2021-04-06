2f5e9aadd29eb1259ff1611e49dcbc4c761a1d86
Adriano Nascimento <ns.adriano@gmail.com>
2021-04-06 09:48:10 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Adriano");
+		
+		System.out.println(obj);
+
+	}
+
+}
